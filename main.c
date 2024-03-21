8e7acfd817be669af4078b204715ffe59d7a5a75
hananjuu <162300456+hananjuu@users.noreply.github.com>
2024-03-21 23:28:05 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+#include <stdio.h>
 #include "header.h"
 
 int main() {
@@ -7,57 +11,55 @@ int main() {
     do {
         if (displayMenu) {
             printf("\nMenu Principal :\n");
-            printf("1. Ajouter une t‚che\n");
-            printf("2. Modifier une t‚che\n");
-            printf("3. Supprimer une t‚che\n");
-            printf("4. Afficher toutes les t‚ches\n");
-            printf("5. Filtrer les t‚ches par prioritÈ\n");
-            printf("6. Marquer une t‚che comme terminÈe\n");
+            printf("1. Ajouter une t√¢che\n");
+            printf("2. Modifier une t√¢che\n");
+            printf("3. Supprimer une t√¢che\n");
+            printf("4. Afficher toutes les t√¢ches\n");
+            printf("5. Filtrer les t√¢ches par priorit√©\n");
+            printf("6. Marquer une t√¢che comme termin√©e\n");
             printf("7. Quitter\n");
             printf("Entrez votre choix : ");
         }
-
-        // VÈrifier si la saisie est un entier
-        if (scanf("%d", &choice) != 1) {
-            printf("Choix invalide. Veuillez rÈessayer.\n");
-            while (getchar() != '\n'); // Vider le tampon d'entrÈe
-            displayMenu = 1;
-            continue; // Revenir au dÈbut de la boucle
-        }
+        scanf("%d", &choice);
+        
 
         switch (choice) {
             case 1:
                 addTask();
                 displayMenu = 1;
+
                 break;
             case 2:
                 modifyTask();
                 displayMenu = 1;
+
                 break;
             case 3:
                 deleteTask();
                 displayMenu = 1;
+
                 break;
             case 4:
                 displayTasks();
-                displayMenu = 0;
+                displayMenu = 0; // Ne pas r√©afficher le menu principal
                 break;
             case 5:
                 filterTasksByPriority();
-                displayMenu = 0;
+                displayMenu = 0; // Ne pas r√©afficher le menu principal
                 break;
             case 6:
                 markTaskAsFinished();
                 displayMenu = 1;
+
                 break;
             case 7:
                 printf("Fin du programme.\n");
                 break;
             default:
-                printf("Choix invalide. Veuillez rÈessayer.\n");
+                printf("Choix invalide. Veuillez r√©essayer.\n");
                 displayMenu = 1;
         }
-    } while (choice <7 && choice>0);
+    } while (choice <= 7&& choice >0);
 
     return 0;
 }
